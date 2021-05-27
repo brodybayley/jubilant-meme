0d221076ee51c599dfed461ac0e125863ff2fe6c
brodybayley <brodybayley@gmail.com>
2021-05-26 17:36:57 -0700

    Add tree java challenge

---
+++ challenges/Java/Tree.java
@@ -0,0 +1,16 @@
+public class Tree {
+  public static void main(String[] args) {
+    //print a tree to the screen
+
+    System.out.println("Hey there, I'm Brody and I’m learning to code in Java!");
+
+    System.out.println("I’m going to plant a tree today!");
+
+    System.out.println(" * ** * ");
+    System.out.println(" ****** ");
+    System.out.println("  **** ");
+    System.out.println("   **  ");
+    System.out.println("   **  "); 
+
+  }
+}
